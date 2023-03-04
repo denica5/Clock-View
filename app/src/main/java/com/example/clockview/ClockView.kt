2fd64d20d2eab2d8a40b5cf5aacc8b3d14872b37
Gleb Rudko <gleb.rudko@bk.ru>
2023-03-04 16:12:27 +0300

    draw hands

--- app/src/main/java/com/example/clockview/ClockView.kt
+++ app/src/main/java/com/example/clockview/ClockView.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.os.Handler
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
-import android.widget.TextClock
 import androidx.core.content.ContextCompat
-import java.time.Clock
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.min
@@ -18,10 +21,11 @@ class ClockView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : TextClock(context, attrs, defStyleAttr) {
-//     lateinit var mClock: Clock
+) : View(context, attrs, defStyleAttr) {
 
 
+    private var mHandler: Handler = Handler()
+    private lateinit var mTimeUpdater: Runnable
     private val paintFillCircle = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
         color = ContextCompat.getColor(context, R.color.gray_background)
@@ -45,27 +49,46 @@ class ClockView @JvmOverloads constructor(
         strokeWidth = 4f
         color = ContextCompat.getColor(context, R.color.pink_mark)
     }
+    private val paintHandHour = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+    }
 
 
-//    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-//        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-//
-//
-//    }
+    private var padding = 0f
 
-
-    //    private var mSeconds = 0f
-//    private var mMinutes = 0f
-//    private var mHour = 0f
-    private var mRadius = 0f
-
-    //    private var mChanged = false
+    private var seconds = 0
+    private var minutes = 0f
+    private var hours = 0f
+    private var radius = 0f
     private var centerWidth = 0
     private var centerHeight = 0
 
+
+    init {
+        mTimeUpdater = object : Runnable {
+            override fun run() {
+                // update clock time data
+                val currentTime: String =
+                    SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(Date())
+                setClockTime(
+                    currentTime.substring(0, 2).toFloat() % 12,
+                    currentTime.substring(3, 5).toFloat(),
+                    currentTime.substring(6, 8).toInt()
+                )
+
+                mHandler.postDelayed(this, 1000)
+            }
+        }
+    }
+
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        mRadius = (min(width, height) / 2.0 * 0.8).toFloat()
+        centerWidth = width / 2
+        centerHeight = height / 2
+        radius = (min(width, height) / 2.0 * 0.8).toFloat()
+        padding = radius / 75
+
+
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -74,26 +97,44 @@ class ClockView @JvmOverloads constructor(
         canvas.drawCircle(
             (centerWidth).toFloat(),
             (centerHeight).toFloat(),
-            mRadius,
+            radius,
             paintFillCircle
         )
-        val minuteMarkLength = mRadius / 30f
+        val minuteMarkLength = radius / 30f
+        val minutePadding =
+            repeat(60) {
+                if (it % 5 != 0) {
+                    drawMarks(canvas, 6, paintMarkMinutes, it, padding, minuteMarkLength)
 
-        repeat(60) {
-            if (it % 5 != 0) {
-                drawMarks(canvas, 6, paintMarkMinutes, it, 6f, minuteMarkLength)
-
+                }
             }
-        }
-        val hourMarkLengthPink = mRadius / 15f
+        val hourMarkLengthPink = radius / 15f
+        Log.d("Check", "$radius")
         repeat(12) {
             if (it % 3 == 0) {
-                drawMarks(canvas, 30, paintMarkHoursPink, it, 6f, hourMarkLengthPink)
+                drawMarks(canvas, 30, paintMarkHoursPink, it, padding, hourMarkLengthPink)
             } else {
-                drawMarks(canvas, 30, paintMarkHoursBlue, it, 6f, minuteMarkLength)
+                drawMarks(canvas, 30, paintMarkHoursBlue, it, padding, minuteMarkLength)
             }
         }
 
+        drawHands(
+            canvas,
+            radius / 1.7f,
+            hours,
+            radius / 50,
+            R.color.gray_hand,
+            STEP_30
+        )
+        drawHands(
+            canvas,
+            radius / 2.6f,
+            minutes,
+            radius / 75,
+            R.color.gray_hand,
+            STEP_6
+        )
+        drawHands(canvas, radius / 3.3f, seconds.toFloat(), radius / 100, ContextCompat.getColor(context,R.color.pink_mark), STEP_6)
     }
 
     private fun drawMarks(
@@ -105,31 +146,119 @@ class ClockView @JvmOverloads constructor(
         markLength: Float,
 
         ) {
+        val coordinates = calculateMarksCoordinates(padding, markLength, step, position)
         canvas.drawLine(
-            centerWidth + (mRadius - padding) * cos(((90 - step * position) * FROM_RAD_TO_DEGREES).toFloat()),
-            centerHeight - (mRadius - padding) * sin(((90 - step * position) * FROM_RAD_TO_DEGREES).toFloat()),
-            centerWidth + (mRadius - markLength - padding) * cos(
-                ((90 - step * position) * FROM_RAD_TO_DEGREES).toFloat()
-            ),
-            centerHeight - (mRadius - markLength - padding) * sin(
-                ((90 - step * position) * FROM_RAD_TO_DEGREES).toFloat()
-            ),
+            coordinates[0],
+            coordinates[1],
+            coordinates[2],
+            coordinates[3],
             paint
         )
-
     }
+
+    private fun drawHands(
+        canvas: Canvas,
+        handLength: Float,
+        position: Float,
+        width: Float,
+        color: Int,
+        step: Int
+
+
+    ) {
+        paintHandHour.color = color
+        paintHandHour.strokeWidth = width
+        val coordinates = calculateCoords(step, position, handLength)
+        canvas.drawLine(
+            centerWidth.toFloat(),
+            centerHeight.toFloat(),
+            coordinates[0],
+            coordinates[1],
+            paintHandHour
+        )
+    }
+
+    private fun calculateMarksCoordinates(
+        padding: Float,
+        markLength: Float = 0f,
+        step: Int,
+        position: Int,
+
+        ): ArrayList<Float> {
+        val result = ArrayList<Float>(4)
+
+        result.add(
+            0,
+            centerWidth + (radius - padding) * cos(((90 - step * position) * FROM_RAD_TO_DEGREES).toFloat())
+        )
+        result.add(
+            1,
+            centerHeight - (radius - padding) * sin(((90 - step * position) * FROM_RAD_TO_DEGREES).toFloat())
+        )
+        result.add(
+            2,
+            centerWidth + (radius - markLength - padding) * cos(
+                ((90 - step * position) * FROM_RAD_TO_DEGREES).toFloat()
+            )
+        )
+        result.add(
+            3,
+            centerHeight - (radius - markLength - padding) * sin(((90 - step * position) * FROM_RAD_TO_DEGREES).toFloat())
+        )
+        return result
+    }
+
+    private fun calculateCoords(
+        step: Int,
+        position: Float,
+        endPosition: Float
+    ): ArrayList<Float> {
+        val result = ArrayList<Float>(2)
+        result.add(
+            0,
+            centerWidth + (radius - padding - endPosition) * cos(((90 - step * position) * FROM_RAD_TO_DEGREES).toFloat())
+        )
+        result.add(
+            1,
+            centerHeight - (radius - padding - endPosition) * sin(((90 - step * position) * FROM_RAD_TO_DEGREES).toFloat())
+        )
+        return result
+    }
+
 
 //    private fun drawNumbers(
 //        canvas: Canvas,
 //        number: Number,
 //        padding: Float
+
 //
 //    ) {
 ////        canvas.drawText(number.toChar(), )
 //    }
 
+    // set clock hand
+    private fun setClockTime(hour: Float, minute: Float, second: Int) {
+        hours = hour + (minute / 60)
+
+        minutes = minute
+        seconds = second
+        invalidate()
+    }
+
+    // start clock
+    fun startClock() {
+        mHandler.post(mTimeUpdater)
+    }
+
+    // stop clock
+    fun stopClock() {
+        mHandler.removeCallbacks(mTimeUpdater)
+    }
+
     companion object {
         private const val FROM_RAD_TO_DEGREES = PI / 180
-
+        private const val START_ANGLE = 90
+        private const val STEP_6 = 6
+        private const val STEP_30 = 30
     }
 }
